10a3e946677bb923f044d069329a6c76440cf146
Aisyah Shadrina <aisyahshadrina@gmail.com>
2019-07-04 09:20:25 +0700

    Initial Commit

---
+++ bukuSaya/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bukuSaya
+//
+//  Created by Aisyah Shadrina on 04/07/19.
+//  Copyright © 2019 Aisyah Shadrina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
